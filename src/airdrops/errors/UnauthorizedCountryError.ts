f63e8930e1343cfc9f51018b302a9c1f875fcf4f
aurelticot <aurelticot.dev@gmail.com>
2024-04-30 15:18:33 +1000

    feat: improve message for unauthorised country

--- src/airdrops/errors/UnauthorizedCountryError.ts
+++ src/airdrops/errors/UnauthorizedCountryError.ts
@@ -3,7 +3,7 @@ import { FunctionalError } from "../../common";
 export class UnauthorizedCountryError extends FunctionalError {
   constructor(
     message = "UnauthorizedCountryError",
-    userMessage = "Your country is not authorized",
+    userMessage = "Unfortunately, your country is not eligible",
     options?: ErrorOptions
   ) {
     super("UnauthorizedCountryError", message, userMessage, options);
