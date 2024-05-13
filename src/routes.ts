a0188cca2e144bb7707f1a6e23a5b1a23989b13d
aurelticot <aurelticot.dev@gmail.com>
2024-05-13 12:37:13 +1000

    feat: set version endpoint at root

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,9 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import express from "express";
 import { routerV1 as airdropsRouterV1 } from "./airdrops";
-import { router as versionRouter } from "./version";
 
 const routerV1 = express.Router();
 routerV1.use("/airdrops", airdropsRouterV1);
 
 export const appRouter = express.Router();
-appRouter.use("/_version", versionRouter);
 appRouter.use("/v1", routerV1);
--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import serverless from "serverless-http";
+import { router as versionRouter } from "./version";
 import { appRouter } from "./routes";
 
 const app = express();
@@ -15,6 +16,7 @@ app.use(cors(corsConfig));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use("/_version", versionRouter);
 app.use("/api/rest", appRouter);
 
 export const handler = serverless(app);
